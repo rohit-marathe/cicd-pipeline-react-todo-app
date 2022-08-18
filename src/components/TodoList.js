593a70ed533e3262b05d0224c049f2d9a8e62729
rohit-marathe <rohitmarathe7989@gmail.com>
2022-08-18 10:10:22 +0530

    file is updated

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -40,7 +40,7 @@ function TodoList() {
 
     return (
         <div>
-            <h1>What's the Plan for Today?</h1>
+            <h1>How Many Plan for Today?</h1>
             <TodoForm onSubmit={addTodo} />
             <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo} />
         </div>
